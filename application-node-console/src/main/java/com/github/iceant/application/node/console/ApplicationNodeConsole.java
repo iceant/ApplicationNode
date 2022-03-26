5fb8709edd0d0413ff888fa919c372391b71f0e3
Chen Peng <pizer.chen@gmail.com>
2022-03-26 23:18:17 +0800

    定制代码生成

--- application-node-console/src/main/java/com/github/iceant/application/node/console/ApplicationNodeConsole.java
+++ application-node-console/src/main/java/com/github/iceant/application/node/console/ApplicationNodeConsole.java
@@ -1,14 +1,11 @@
 package com.github.iceant.application.node.console;
 
-import com.github.iceant.application.node.console.storage.mapper.TMenuMapper;
-import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.Banner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.ConfigurableApplicationContext;
 
 @SpringBootApplication
-@MapperScan(basePackageClasses = {TMenuMapper.class})
 public class ApplicationNodeConsole {
     public static void main(String[] args) {
         ConfigurableApplicationContext ctx = new SpringApplicationBuilder(ApplicationNodeConsole.class)
--- application-node-console/src/main/java/com/github/iceant/application/node/console/config/MyBatisPlusConfig.java
+++ application-node-console/src/main/java/com/github/iceant/application/node/console/config/MyBatisPlusConfig.java
@@ -1,8 +1,11 @@
 package com.github.iceant.application.node.console.config;
 
+import com.github.iceant.application.node.console.storage.mapper.TMenuMapper;
+import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
+@MapperScan(basePackageClasses = {TMenuMapper.class})
 public class MyBatisPlusConfig {
 //    @Bean
 //    public GlobalConfig globalConfig() {
